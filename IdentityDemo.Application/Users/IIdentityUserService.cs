53570598d1dcbc561f2a069ef1e555328e689a9a
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-06-05 13:46:15 +0200

    Add user service interface and implementation
    Created `IIdentityUserService` interface with methods for user creation and sign-in. Implemented `IdentityUserService` class to manage user operations using `UserManager`, `SignInManager`, and `RoleManager`. Added `CreateUserAsync` method for user creation and stubbed `SignInAsync` method.

---
+++ IdentityDemo.Application/Users/IIdentityUserService.cs
@@ -0,0 +1,10 @@
+﻿using IdentityDemo.Application.Dtos;
+
+namespace IdentityDemo.Application.Users;
+
+public interface IIdentityUserService
+{
+    Task<UserResultDto> CreateUserAsync(UserProfileDto user, string password);
+
+    Task<UserResultDto> SignInAsync(string email, string password);
+}
--- IdentityDemo.Infrastructure/IdentityUserService.cs
+++ IdentityDemo.Infrastructure/IdentityUserService.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using IdentityDemo.Application.Dtos;
+using IdentityDemo.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
+using IdentityDemo.Application.Users;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IdentityDemo.Infrastructure
+namespace IdentityDemo.Infrastructure;
+
+public class IdentityUserService
+    (
+    UserManager<ApplicationUser> userManager,
+    SignInManager<ApplicationUser> signInManager,
+    RoleManager<ApplicationUser> roleManager
+    ) : IIdentityUserService
 {
-    internal class IdentityUserService
+    public async Task<UserResultDto> CreateUserAsync(UserProfileDto user, string password)
     {
+        var result = await userManager.CreateAsync( new ApplicationUser
+        {
+            UserName = user.Email,
+            Email = user.Email,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+        }, password);
+
+        return new UserResultDto(result.Errors.FirstOrDefault()?.Description);
+    }
+
+    public Task<UserResultDto> SignInAsync(string email, string password)
+    {
+        throw new NotImplementedException();
     }
 }
